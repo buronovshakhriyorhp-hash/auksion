f7c434b5eb9e0249cbd7fbfe5b83865c3e22c6fa
Shakhriyor <buronovshakhriyorhp@gmail.com>
2026-05-21 14:00:07 +0500

    loyiha tayyor

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * MathOyin | Premium Auction Platform
+ * Author : Jamshid Eshpulatov
+ * Version: 2.0.0
+ *
+ * NOTE: Barcha mantiq index.html ichidagi <script> blokida joylashgan.
+ * Bu fayl eski loyihadan qolgan bo'lib, hozir ishlatilmaydi.
+ * Kelajakda kodni modularga ajratish uchun shu joydan foydalanish mumkin.
+ */
